9e19a5ef3655ccb7619be7ba6a067977e0d9c005
justnotanu <prernapatel1446@gmail.com>
2024-07-16 17:29:26 +0530

    Time: 118 ms (80.83%), Space: 112.7 MB (7.80%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,27 +1,25 @@
 class Solution {
-    public double findMaxAverage(int[] nums, int k) {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
         int sum = 0;
-        
-        // Calculate sum of the first k elements
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i<k; i++)
             sum += nums[i];
-        }
         
         int maxSum = sum;
         
         int startIndex = 0;
-        int endIndex = k;
-        
-        // Sliding window approach to find maximum sum of k consecutive elements
-        while (endIndex < nums.length) {
-            sum -= nums[startIndex];   // Remove the element going out of the window
+        int endIndex =k;
+        while (endIndex < nums.size()) {
+            
+            sum -= nums[startIndex];
             startIndex++;
-            sum += nums[endIndex];     // Add the new element coming into the window
+            sum += nums[endIndex];
             endIndex++;
-            maxSum = Math.max(maxSum, sum);
+            maxSum = max(maxSum,sum);
+        }
+            
+            return(double) maxSum / k;
         }
         
-        // Calculate the maximum average and return it
-        return (double) maxSum / k;
-    }
-}
+    
+};
