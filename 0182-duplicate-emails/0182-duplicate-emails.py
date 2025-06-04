5dfee43388c9426fb8735e4be157bc4b23e5845d
ANU PRIYA <prernapatel1446@gmail.com>
2025-06-04 06:14:43 +0530

    Time: 279 ms (53.22%), Space: 67.1 MB (73.44%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
+    results = pd.DataFrame()
+
+    results = person.loc[person.duplicated(subset=["email"]), ["email"]]
+
+    return results.drop_duplicates()
